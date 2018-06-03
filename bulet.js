b5ae098ead144ee60889712561703ac559993d2e
hristoCh8 <hchipev@abv.bg>
2018-06-03 18:33:27 +0300

    bullet

---
+++ bulet.js
@@ -0,0 +1,20 @@
+﻿class Bullet{
+    constructor(x_, y_, tx, ty,color_){
+        this.mcolor=color_
+        this.x = x_;
+        this.y = y_;
+        let dist = Math.sqrt((tx-x_)*(tx-x_) + (ty-y_)*(ty-y_));
+        this.dx = (tx-x_)/dist*3;
+        this.dy = (ty-y_)/dist*3;
+    }
+    update(){
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+    draw(){  
+        context.fillStyle = this.mcolor;
+        context.beginPath()
+        context.arc(this.x, this.y,10,0,Math.PI*2); 
+        context.fill()
+    }
+};
---
+++ enemy2.js
@@ -0,0 +1,27 @@
+﻿// Creating variables
+class Enemy{
+    constructor(x_,y_,size_,px_,py_,colors_,tx_,ty_){
+        this.px=px_;
+        this.py=py_;
+        this.x=x_;
+        this.y=y_;
+        this.size=size_
+        this.colors=colors_;
+        this.tx=tx_
+        this.ty=ty_
+    }
+    draw(){
+        context.beginPath()
+        context.arc(this.x,this.y,this.size,0,Math.PI*2)
+        context.strokeStyle=this.colors
+        context.stroke()
+        
+    }
+    move(){
+   let dist = Math.sqrt((this.tx - this.x)*(this.tx - this.x) + (this.ty - this.y)*(this.ty - this.y));
+        this.x += (this.tx - this.x)/dist*2.5;
+        this.y +=(this.ty - this.y)/dist*2.5;
+    }
+    
+}
+    
